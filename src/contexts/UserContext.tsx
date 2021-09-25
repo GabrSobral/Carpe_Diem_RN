a0b43a177f08637950ee5cd12359d2fb6c45f6d3
Sobraloser <Asgoth55@gmail.com>
2021-09-25 11:07:09 -0300

    feat: add user context

---
+++ src/contexts/UserContext.tsx
@@ -0,0 +1,148 @@
+import React,{ useCallback } from "react";
+import { createContext, ReactNode, useContext, useState, useEffect } from "react";
+import { api } from "../services/api";
+import { User } from "../types/user";
+import { getToken, removeToken, setToken } from "../utils/handleToken";
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+interface SignProps {
+  name?: String;
+  email: String;
+  password: String;
+  query?: '/login' | '/users'
+}
+interface SignResult {
+  data: {
+    user: {
+      id: String;
+      name: String;
+      email: String;
+      created_at: Date;
+      updated_at: Date;
+    };
+    token: String;
+  };
+  message?: String;
+}
+interface UserContextProps {
+  Sign: ({name, password, email, query}: SignProps) => any;
+  username: String;
+  Logout: () => Promise<unknown>;
+  user?: User;
+  handleFinishActivityInUser: () => void;
+  setHasAnswered: () => void;
+  // updateUserState: () => void;
+}
+
+const UserContext = createContext({} as UserContextProps)
+
+export function UserProvider({ children }: UserProviderProps){
+  const [ username, setUsername ] = useState('')
+  const [ user, setUser ] = useState<User>()
+
+  // const updateUserState = useCallback(async () => {
+  //   const userStore = await storage.get('user')
+  //   if(userStore){
+  //     setUser(userStore)
+  //     if(userStore){
+  //       const firstName = userStore.name.split(' ')[0]
+
+  //       console.log(firstName)
+  //       setUsername(firstName)
+  //     }
+  //   }
+  // },[])
+
+  // useEffect(() => {
+  //   (async () => {
+  //     if(getToken()) {
+  //       setIsAuthenticated(true) 
+  //       updateUserState()
+  //     }
+  //   })()
+  // },[ updateUserState ])
+
+
+  async function Sign({name, email, password, query = '/login'}: SignProps) {
+    const result = {} as SignResult
+
+    try {
+      const { data } = await api.post(query, { name, email, password })
+      
+      setToken(data.token)
+      // await storage.set('user', data.user)
+      setUser(data.user)
+      const firstName = data.user.name.split(' ')[0]
+      setUsername(firstName)
+
+      result.data = data
+      result.message = "ok"
+    } catch(error: any) {
+      result.message = error.response.data.error
+    } finally {
+      return result
+    }
+  }
+
+  function Logout(){
+    return new Promise((resolve, reject) => {
+      // storage.remove('user')
+      // storage.remove('activities')
+      removeToken()
+      setUsername('')
+      setUser(undefined)
+
+      return resolve('ok')
+    })
+  }
+
+  async function handleFinishActivityInUser(){
+    setUser(prev => {
+      if(prev){
+        prev.all_activities_finished++
+        prev.activities_finished_today++
+
+        // (async () => {
+        //   await storage.set('user', prev)
+        // })()
+
+        return prev
+      }
+      return undefined
+    })
+  }
+
+  function setHasAnswered(){
+    setUser(prev => {
+      if(prev) {
+        (async () => {
+          prev.hasAnswered = true
+          // await storage.set('user', prev)
+        })()
+        return prev
+      }
+      return undefined
+    })
+  }
+
+  return(
+    <UserContext.Provider 
+      value={{
+        Sign,
+        username,
+        Logout,
+        user,
+        handleFinishActivityInUser,
+        setHasAnswered,
+        // updateUserState
+      }}>
+      {children}
+    </UserContext.Provider>
+  )
+}
+
+export function useUsers(){
+  return useContext(UserContext)
+}
