d56cc35d66ce77f2156630990e898c959e2476b4
Sobraloser <Asgoth55@gmail.com>
2021-09-26 22:25:31 -0300

    feat: finish player structure

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -4,13 +4,28 @@ import { View, Text, TouchableOpacity } from 'react-native'
 
 import { styles }  from './style'
 import { theme } from "../../styles/theme";
+import { useState } from "react";
 
 export function Player(){
+  const [ isStarted, setIsStarted ] = useState(false)
+
   return(
     <View style={styles.container}>
-      <TouchableOpacity style={styles.controlButton}>
-        <MaterialIcons name="play-arrow" size={32} color={theme.colors.white}/>
+      <TouchableOpacity 
+        style={styles.controlButton} 
+        onPress={() => setIsStarted(!isStarted)}
+      >
+        { isStarted ? 
+          <MaterialIcons name="play-arrow" size={32} color={theme.colors.white}/> :
+          <MaterialIcons name="pause" size={32} color={theme.colors.white}/> 
+        }
       </TouchableOpacity>
+
+      <View style={styles.sliderContainer}>
+        <Text style={styles.duration}>00:23</Text>
+        <View style={styles.slider}/>
+        <Text style={styles.duration}>04:16</Text>
+      </View>
     </View>
   )
 }
--- src/components/Player/style.ts
+++ src/components/Player/style.ts
@@ -6,6 +6,8 @@ export const styles = StyleSheet.create({
     width: '100%',
     borderRadius: 7,
     padding: 8,
+    flexDirection: 'row',
+    alignItems: 'center',
     backgroundColor: theme.colors.blue300
   },
   controlButton: {
@@ -15,5 +17,21 @@ export const styles = StyleSheet.create({
     backgroundColor: theme.colors.blue600,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  sliderContainer: {
+    padding: 6,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  duration: {
+    color: theme.colors.white
+  },
+  slider: {
+    height: 3,
+    flex: 1,
+    marginHorizontal: 5,
+    backgroundColor: theme.colors.green300
   }
 })
