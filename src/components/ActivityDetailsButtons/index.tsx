7f46e84ac5467e57b5bff0c3077625df0e386cb3
Sobraloser <Asgoth55@gmail.com>
2021-09-26 22:25:04 -0300

    feat: add modal to activityDetailsButtons

--- src/components/ActivityDetailsButtons/index.tsx
+++ src/components/ActivityDetailsButtons/index.tsx
@@ -1,16 +1,42 @@
-import React from 'react'
-import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import React, { useState } from 'react'
+import { RectButton } from "react-native-gesture-handler";
 import { Text, View } from 'react-native'
+
+import { ModalComponent } from '../Modal'
 import { styles } from './style'
 
 export function ActivityDetailsButtons(){
+  const [ isFinishModalVisible, setIsFinishModalVisible ] = useState(false)
+  const [ isDenyModalVisible, setIsDenyModalVisible ] = useState(false)
+
   return (
     <View style={styles.handleButtonsContainer}>  
-      <RectButton style={[styles.handleButton, styles.reject]}>
+      <ModalComponent 
+        title="Parabéns!"
+        description="Você conseguiu realizar uma tarefa, isso é ótimo!"
+        isVisible={isFinishModalVisible} 
+        closeModal={() => setIsFinishModalVisible(false)}
+      />
+
+      <ModalComponent 
+        title="Oh não..."
+        description="Você tem certeza de que deseja descartar essa tarefa?"
+        isVisible={isDenyModalVisible}
+        dualButtons 
+        closeModal={() => setIsDenyModalVisible(false)}
+      />
+
+      <RectButton 
+        style={[styles.handleButton, styles.reject]}
+        onPress={() => setIsDenyModalVisible(true)}
+      >
         <Text style={styles.handleText}>Descartar</Text>
       </RectButton>
 
-      <RectButton style={[styles.handleButton, styles.confirm]}>
+      <RectButton 
+        style={[styles.handleButton, styles.confirm]} 
+        onPress={() => setIsFinishModalVisible(true)}
+      >
         <Text style={styles.handleText}>Concluir</Text>
       </RectButton>
     </View>
