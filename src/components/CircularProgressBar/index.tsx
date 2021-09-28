b9de3ad5a7538a0218d60038a6c31dce16e26496
Sobraloser <Asgoth55@gmail.com>
2021-09-27 21:19:18 -0300

    feat: transfer circularBar to components

---
+++ src/components/CircularProgressBar/index.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import { View } from 'react-native'
+import CircularProgress from 'react-native-circular-progress-indicator';
+
+import { useUsers } from '../../contexts/UserContext';
+import { theme } from '../../styles/theme';
+
+import { styles } from './style'
+
+export function CircularProgressBar(){
+  const { user } = useUsers()
+  const [ percentage, setPercentage ] = useState<number>(0)
+
+  useEffect(() => {
+    const percentegeCalculated = Math.round(
+      ((user?.activities_finished_today || 0) * 100) / (user?.quantity_of_activities || 0))
+    setPercentage(percentegeCalculated)
+  },[ user ])
+
+  return(
+    <View style={styles.progressBarContainer}>
+      <CircularProgress 
+        value={percentage || 0}
+        radius={100}
+        maxValue={100}
+        textColor={theme.colors.text}
+        fontSize={40}
+        valueSuffix={'%'}
+        activeStrokeWidth={7}
+        activeStrokeColor={theme.colors.green300}
+        inActiveStrokeWidth={4}
+        inActiveStrokeColor={theme.colors.green300}
+        inActiveStrokeOpacity={0.5}
+      />
+    </View>
+  )
+}
---
+++ src/components/CircularProgressBar/style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  progressBarContainer: {
+    position: 'relative',
+    width: '100%',
+    height: 200,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 16
+  },
+})
