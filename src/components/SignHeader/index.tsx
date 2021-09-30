e81c84daae274a8262e4ae5d2c721fbf2696d40c
Sobraloser <Asgoth55@gmail.com>
2021-09-29 23:53:45 -0300

    feat: add expo-av and icon to input

--- src/components/SignHeader/index.tsx
+++ src/components/SignHeader/index.tsx
@@ -6,16 +6,18 @@ import { useNavigation } from '@react-navigation/native'
 import { styles } from './style'
 
 interface SignHeaderProps{
-  title: "Entrar" | "Cadastrar";
+  title: string;
+  button?: "Entrar" | "Cadastrar" | "Voltar"
 }
 
-export function SignHeader({ title }: SignHeaderProps){
+export function SignHeader({ title, button }: SignHeaderProps){
   const { navigate, goBack } = useNavigation()
 
   function handleNavigation(){
-    switch(title){
-      case "Cadastrar": goBack(); break;
-      case "Entrar" : navigate('SignUp'); break;
+    switch(button){
+      case "Cadastrar" : navigate('SignUp'); break;
+      case "Entrar" : goBack(); break;
+      case "Voltar" : goBack(); break;
     }
   }
 
@@ -23,14 +25,16 @@ export function SignHeader({ title }: SignHeaderProps){
     <View style={styles.header}>
       <Text style={styles.title}>{title}</Text>
 
-      <RectButton 
-        style={styles.Button}
-        onPress={handleNavigation}
-      >
-        <Text style={styles.titleButton}>
-          {title === "Entrar" ? "Cadastrar" : "Entrar"}
-        </Text>
-      </RectButton>
+      { button &&
+        <RectButton 
+          style={styles.Button}
+          onPress={handleNavigation}
+        >
+          <Text style={styles.titleButton}>
+            {button}
+          </Text>
+        </RectButton>
+      }
     </View>
   )
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,16 +10,26 @@ import { useUsers } from '../contexts/UserContext'
 import { Clock } from '../screens/Clock'
 import { BottomTabs } from './bottomTabs'
 import { ActivityDetails } from '../screens/ActivityDetails'
+import { ChangePassword } from '../screens/ChangePassword'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { MyFeedbacks } from '../screens/MyFeedbacks'
 
 export function Routes(){
   const { user } = useUsers()
+  const [ answered, setAnswered ] = useState(false)
   const { Navigator, Screen } = createStackNavigator()
+
+  useEffect(() => {
+    if(user?.hasAnswered)
+      return setAnswered(true)
+  },[user?.hasAnswered])
 
   return (
     <NavigationContainer>
       <Navigator 
         screenOptions={{ headerShown: false }} 
-        initialRouteName={user?.hasAnswered ? "BottomTabs" : "Questionnaire"}
+        initialRouteName={ answered ? "Questionnaire" : "BottomTabs"}
       >
         { !user ? 
           <>
@@ -28,10 +38,12 @@ export function Routes(){
           </>
           :
           <>
-            <Screen name="BottomTabs"            component={BottomTabs}/>
+            <Screen name="BottomTabs"      component={BottomTabs}/>
             <Screen name="Questionnaire"   component={Questionnaire}/>
             <Screen name="Clock"           component={Clock}/>
             <Screen name="ActivityDetails" component={ActivityDetails}/>
+            <Screen name="ChangePassword" component={ChangePassword}/>
+            <Screen name="MyFeedbacks" component={MyFeedbacks}/>
           </>
          }
       </Navigator>   
